aa9ec5b357f9dd945da63e490b7f839434eed0f7
melanee <158222842+EhmAesch@users.noreply.github.com>
2025-10-28 15:59:26 -0700

    completed join strings by learning to add strings together, then today (10/28) worked on completing repeat strings by figuring out how to set empty strings (let string =) then how to write a for loop that starts counter at zero, runs an amount of iterations based on parameters, then adds in increments of one (i=0; i<times; i++) , then learned how to add the output to a string (string+=word) word being in the parameters, then to return the new string. this is based on the code having determined the 'word' and 'times' that each iteration needs to complete. This one was challenging, but working from the answer backwards and decoding what each section of code meant was helpful in seeing how the end functions gave us the answer 10.28 357PM

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -1,13 +1,26 @@
 /*
- First name: Carlos
- Last name: Stevenson
- This year: 1965
- Birth year: 1947
+ First name: Ehm
+ Last name: Aesch
+ This year: 2025
+ Birth year: 1994
 
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
+ The greeting will read: Hello! My name is Ehm Aesch and I am 31 years old. 
 */
 
 // Add your code right below, good luck!
+
+const birthYear = 1994;
+const thisYear = 2025;
+const firstName = "Ehm";
+const lastName = "Aesch";
+
+const fullName = firstName + " " + lastName;
+const age = (thisYear - birthYear); 
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old."
+
+
+
 
 
 
@@ -21,5 +34,5 @@ module.exports = {
   birthYear: typeof birthYear === 'undefined' ? undefined : birthYear,
   greeting: typeof greeting === 'undefined' ? undefined : greeting,
   fullName: typeof fullName === 'undefined' ? undefined : fullName,
-  age: typeof age === 'undefined' ? undefined : age
+  age: typeof age === 'undefined' ? undefined : age,
 }
--- 05_joinStrings/joinStrings.spec.js
+++ 05_joinStrings/joinStrings.spec.js
@@ -1,28 +1,28 @@
 const values = require('./joinStrings')
 
 describe('step 2', () => {
-  test('firstName is Carlos', () => {
-    expect(values.firstName).toEqual('Carlos');
+  test('firstName is Ehm', () => {
+    expect(values.firstName).toEqual('Ehm');
   });
-  test.skip('lastName is Stevenson', () => {
-    expect(values.lastName).toEqual('Stevenson');
+  test('lastName is Aesch', () => {
+    expect(values.lastName).toEqual('Aesch');
   });
-  test.skip('thisYear is 1965', () => {
-    expect(values.thisYear).toEqual(1965);
+  test('thisYear is 2025', () => {
+    expect(values.thisYear).toEqual(2025);
   });
-  test.skip('birthYear is 1947', () => {
-    expect(values.birthYear).toEqual(1947);
+  test('birthYear is 1994', () => {
+    expect(values.birthYear).toEqual(1994);
   });
-  test.skip('greeting is properly output', () => {
-    expect(values.greeting).toEqual('Hello! My name is Carlos Stevenson and I am 18 years old.');
+  test('greeting is properly output', () => {
+    expect(values.greeting).toEqual('Hello! My name is Ehm Aesch and I am 31 years old.');
   });
 });
 
 describe('step 3', () => {
-  test.skip('fullName is Carlos Stevenson', () => {
-    expect(values.fullName).toEqual('Carlos Stevenson');
+  test('fullName is Ehm Aesch', () => {
+    expect(values.fullName).toEqual('Ehm Aesch');
   });
-  test.skip('age is 18', () => {
-    expect(values.age).toEqual(18);
+  test('age is 31', () => {
+    expect(values.age).toEqual(31);
   });
 });
