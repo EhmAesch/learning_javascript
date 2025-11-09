068a7928525bfd4f188cbd2fbc8607736c0708fa
melanee <158222842+EhmAesch@users.noreply.github.com>
2025-11-08 18:50:12 -0800

    learned how to use indexOF to find a value within a parameter, splice() to remove a value, while to use a 'while' loop to rerun the code, and I'm starting to understand the language used to create the code. Whiile I don't find TOP to provide the information in laments terms. Their links are helpful and weirdly enough CGPT has been helpful in deconstructing codes to learn how each part works together. 11.08.25 650pm

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,7 +1,54 @@
-const removeFromArray = function (num,dups) 
+const removeFromArray = function (values, ...remove)
 {
-    
+   remove.forEach(num => {
+        let index;
+        // Keep removing num while it exists in arr
+        while ((index = values.indexOf(num)) !== -1) {
+            values.splice(index, 1);  // Remove the number at index
+        }
+    });
+
+    return values;  // Return the modified array
 }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
